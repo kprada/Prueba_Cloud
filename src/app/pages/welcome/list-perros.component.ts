35eb11f6c872996c8793826510f5d9af2a93f7a9
Kevin Prada <kprada398@gmail.com>
2020-07-24 02:59:55 -0500

    Prueba Cloud

---
+++ src/app/pages/welcome/list-perros.component.ts
@@ -0,0 +1,104 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute} from '@angular/router';
+import { Perros } from '../../models/perros.model';
+import { DogServicesService } from '../../services/dog-services.service';
+import { Razas } from '../../models/razas.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EditModalComponent } from './edit-modal.component';
+import  Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-list-perros',
+  templateUrl: './list-perros.component.html',
+})
+export class ListPerrosComponent implements OnInit {
+
+ lista:Razas;
+ raza:string;
+ mensaje:boolean=false;
+
+
+
+  constructor(private ActivatedRouter: ActivatedRoute,
+    public dogsService:DogServicesService, public modalService:NgbModal) { 
+    this.ActivatedRouter.params.subscribe(params=>{
+     this.getPerros(params['item']);
+     
+    });
+
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+getPerros(i){
+  this.lista=this.dogsService.list[i];
+
+if (this.lista.perros.length<=0) {
+  this.mensaje=true
+}
+}
+
+
+
+  borrar(i:number){
+     
+Swal.fire({
+  title: 'Are you sure?',
+  text: "You won't be able to revert this!",
+  icon: 'warning',
+  showCancelButton: true,
+  allowOutsideClick: false,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  this.lista.perros.splice(i,1)
+  this.dogsService.guardarStorage();
+  if (result.value) {
+    Swal.fire(
+      'Deleted!',
+      'Your file has been deleted.',
+      'success'
+    )
+  }
+})
+
+   
+
+   
+
+  }
+
+
+
+  editPerro(item:Perros){//RECIBO EN EL PARAMETRO EL USUARIO
+    const ref=this.modalService.open(EditModalComponent,{ size: 'lg' }); //se inicializa una constatnte de referencia  con el servicio NgModal y se le pasa el componente donde va a trabajar y como segundo parametro el tamaño del modal
+    ref.componentInstance.item= item; //se pasa en una instancia de referencia de una variable llenandola con el usuario que viene en el parametro de la funcion
+    ref.result.then((yes)=>{ //abre el modal y devuelve una promesa
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Perro Actualizado correctamente',
+        showConfirmButton: false,
+        timer: 1500
+      })
+  
+      this.dogsService.guardarStorage()
+  
+        console.log('ok click');
+  
+     
+  
+    }),(cancel)=>{
+      console.log('cancel click');
+  
+  
+    }
+    
+  
+  
+  }
+
+}
---
+++ src/app/pages/welcome/welcome.component.ts
@@ -0,0 +1,167 @@
+import { Component, OnInit } from '@angular/core';
+import { DogServicesService } from '../../services/dog-services.service';
+import { Razas } from '../../models/razas.model';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Perros } from '../../models/perros.model';
+import { Router } from '@angular/router';
+import  Swal from 'sweetalert2';
+
+
+
+
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent implements OnInit {
+  forma:FormGroup;
+  desde: number=0;
+public list:Razas[]=[];
+public razas:Razas;
+public imagen:string
+isVisible = false;
+isConfirmLoading = false;
+  constructor(public dogService:DogServicesService,
+    private router: Router
+  ) {
+    this.list=this.dogService.list;
+    this.getbreed();
+    this.crearForm();
+    
+
+   }
+
+  ngOnInit() {
+
+  }
+  get  nombreValidador(){
+    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  }
+  get  fechaValidator(){
+    return this.forma.get('fecha_nacimiento').invalid && this.forma.get('fecha_nacimiento').touched;
+  }
+
+
+  crearForm(){
+    this.forma= new FormGroup({
+   nombre:new FormControl(null , [Validators.required, Validators.minLength(5)]),
+   fecha_nacimiento:new FormControl(null , Validators.required),
+  
+    });
+  }
+
+
+
+
+
+
+  getbreed(){
+    this.dogService.getAllBreeds().subscribe((res:any)=>{
+     for (const key in res.message) {
+       if (Object.prototype.hasOwnProperty.call(res.message, key)) {
+         const element = res.message[key];
+       
+       }
+       this.dogService.getImgRandom(key).subscribe((data:any)=>{
+       data.message;
+         this.dogService.crearlista(data.message,key);
+
+       });
+   
+    
+     }
+    
+    });
+
+    
+  }
+
+  getimg(){
+    let beagle='beagle';
+    this.dogService.getImgRandom(beagle).subscribe(data=>{
+      console.log(data);
+      
+    });
+  }
+ 
+
+
+
+  add(razas:Razas){
+  
+    if (this.forma.invalid) {// VALIDA QUE EL FORMULARIO NO SEA INVALIDO
+      Object.values(this.forma.controls).forEach((control) =>{// RECORRE LOS CAMPOS DEL FIRMULARIOS
+  
+        control.markAllAsTouched(); // VERIFICA CADA CAMPO
+        Swal.fire({
+          title:'Campos Obligatorios',
+          icon:'error',
+          allowOutsideClick: false,
+
+        });
+  
+      });
+      return;
+    }
+   let perro:Perros={
+     nombre:this.forma.value.nombre,
+     fecha_nacimiento:this.forma.value.fecha_nacimiento
+   };
+   this.razas.perros.push(perro);
+   console.log(razas);
+   this.dogService.guardarStorage();
+   this.handleOk();
+   Swal.fire({
+    title:'Registro Exitoso',
+    icon:'success'
+  });
+  this.forma.reset();
+  }
+  listarperros(item:any){
+/*     console.log(item);
+ */    
+/* this.router.navigate(['/perros',JSON.stringify(item)]);
+ */this.router.navigate(['/perros',item]);
+
+  }
+
+
+
+
+
+  showModal(item:Razas): void {
+    this.razas=item;
+    this.isVisible = true;
+
+  /*   if (this.forma.invalid) {
+      return;
+    }
+   let perro: Perros ={
+    nombre: this.forma.value.nombre,
+     fecha_nacimiento:this.forma.value.fecha_nacimiento
+   }; */
+   
+/*  console.log(perro);
+ */  }
+
+  handleOk(): void {
+    this.isConfirmLoading = true;
+    setTimeout(() => {
+      this.isVisible = false;
+      this.isConfirmLoading = false;
+    }, 500);
+  }
+
+  handleCancel(): void {
+    this.isVisible = false;
+  }
+
+
+
+
+
+
+
+}
